d9245e10d58136ff211ffcd5fbf77ba7de786290
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-12 16:04:20 +0900

    :sparkles: feat: GlobalErrorBoundary 구현 및 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { GlobalErrorBoundary } from '@/components/boundaries/GlobalErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GlobalErrorBoundary>
+      <Component {...pageProps} />;
+    </GlobalErrorBoundary>
+  );
 }
