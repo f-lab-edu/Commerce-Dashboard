ff5ccf19e3449594e1dbc0aa8104c9d989f9c51d
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-12 23:43:42 +0900

    :recycle: refactor: MSW 브라우저 설정 수정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,4 @@
 import { setupWorker } from 'msw/browser';
-
-// 일단 빈 핸들러로 시작 (다음 이슈에서 추가)
-// eslint-disable-next-line
-export const handlers: any[] = [];
+import { handlers } from './handlers';
 
 export const worker = setupWorker(...handlers);
