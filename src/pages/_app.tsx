376247158e1f05a8d4e1e5a51dedf9372a69ddd4
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-12 16:48:31 +0900

    :sparkles: feat: MSW 브라우저 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,16 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { GlobalErrorBoundary } from '@/components/boundaries/GlobalErrorBoundary';
 
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window !== 'undefined') {
+    import('@/mocks/browser').then(({ worker }) => {
+      worker.start({
+        onUnhandledRequest: 'bypass',
+      });
+    });
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalErrorBoundary>
