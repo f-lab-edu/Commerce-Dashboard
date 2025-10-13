ea5d51b53682ec33d868fc4ab7d5c0dbd9c73316
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-13 11:12:11 +0900

    :sparkles: feat: MSW 핸들러 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,10 +1,16 @@
 import { http, HttpResponse, delay } from 'msw';
-import { mockKPI } from '../data';
+import { mockCategories, mockKPI } from '../data';
 
 export const handlers = [
   http.get('/api/kpi', async () => {
     await delay(500);
 
     return HttpResponse.json(mockKPI);
+  }),
+
+  http.get('/api/insights/categories', async () => {
+    await delay(500);
+
+    return HttpResponse.json(mockCategories);
   }),
 ];
