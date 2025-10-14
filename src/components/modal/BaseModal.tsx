678fa279015b9101c730ac995c143487f19cf317
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-14 15:40:44 +0900

    :recycle: refactor: BaseModal에서 closeModal 호출

--- src/components/modal/BaseModal.tsx
+++ src/components/modal/BaseModal.tsx
@@ -2,6 +2,8 @@ import { ModalState } from '@/types/modal/modal';
 import { Dialog, DialogActions, DialogContent } from '@mui/material';
 import ModalContent from './ModalContent';
 import ModalActions from './ModalActions';
+import { closeModalAtom } from '@/lib/modal/modalAtoms';
+import { useSetAtom } from 'jotai';
 
 interface BaseModalProps {
   modal: ModalState;
@@ -9,14 +11,17 @@ interface BaseModalProps {
 }
 
 export default function BaseModal({ modal, zIndex }: BaseModalProps) {
-  const { options, resolve, reject } = modal;
+  const { id, options, resolve, reject } = modal;
+  const closeModal = useSetAtom(closeModalAtom);
 
   const handleClose = () => {
     reject();
+    closeModal({ id, value: undefined });
   };
 
   const handleConfirm = (value?: unknown) => {
     resolve(value);
+    closeModal({ id, value });
   };
 
   return (
